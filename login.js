2381e04634421e7b0477305308373f04edf06cdf
Fatimah Otuh <fatimahotuh@gmail.com>
2026-03-05 10:14:56 +0000

    modified index.html,login.css,login.html,login.js and testimonial.html

--- login.js
+++ login.js
@@ -1,3 +1,31 @@
-const loginForm
-const login
-const submitButton
+const loginForm = document.querySelector(".login-form");
+const login = document.querySelector(".login");
+const submitButton = document.getElementById("submit");
+const username = document.getElementById("name");
+const password = document.getElementById("password");
+const error = document.querySelector(".error");
+const passwordOpen = document.querySelector(".fa-eye-slash");
+const passwordClose = document.querySelector(".fa-eye");
+submitButton.addEventListener("click", function(e) {
+    e.preventDefault();
+    if (username.value === "" || password.value === "") {
+        error.textContent = "Enter Username and Password"
+    }  else if (password.value.length < 8) {
+        error.textContent = "Enter a stronger Password"
+    } else {
+        alert("Login Successful!!!")
+    }
+
+});
+
+passwordOpen.addEventListener("click", function () {
+   password.type = "text";
+   passwordOpen.style.display = "none";
+   passwordClose.style.display = "block";
+});
+
+passwordClose.addEventListener("click", function () {
+   password.type = "password";
+   passwordClose.style.display = "none";
+   passwordOpen.style.display = "block";
+});
